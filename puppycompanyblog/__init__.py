28bb00bde5be575984de90228e147d9f630c5525
Ayush Pal <pal.ayush58@gmail.com>
2025-03-26 22:00:04 +0530

    Added blog post views and templates.

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -31,7 +31,9 @@ login_manager.login_view = 'users.login'
 from puppycompanyblog.core.views import core
 from puppycompanyblog.error_pages.handler import error_pages
 from puppycompanyblog.users.views import users
+from puppycompanyblog.blog_posts.views import blog_posts
 
 app.register_blueprint(users)
+app.register_blueprint(blog_posts)
 app.register_blueprint(core)
 app.register_blueprint(error_pages)
